83e544f9b439607322dfcc144f8a59d05f77d380
Esteban <emgarcia1@miuandes.cl>
2024-03-29 15:01:52 -0300

    feat:navbar, about and contact works

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,12 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
+  get  "/home", to: "static_pages#home"
+  get  "/about", to: "static_pages#about"
+  get  "/contact", to: "static_pages#contact"
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  root 'static_pages#home'
 end
